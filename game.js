d97b0114bb17c10f08472ad94fe85d31cf26b69c
Crissky <pg2006pe@hotmail.com>
2020-03-25 18:03:00 -0300

    Game base files

---
+++ game.js
@@ -0,0 +1,7 @@
+console.log('[Cris] Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
