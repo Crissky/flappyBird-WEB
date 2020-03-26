bdb388d1d3fc418d122a49e44b12c890bd6a942d
Crissky <pg2006pe@hotmail.com>
2020-03-26 05:12:55 -0300

    Start Screen and "background" name correction

--- game.js
+++ game.js
@@ -1,13 +1,36 @@
 console.log('[Cris] Flappy Bird');
 
+// VARIABLES
 const sprites = new Image();
 sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-// [Backgroud]
-const backgroud = {
+let screenEnabled = {};
+
+// OBJECTS 
+// [MessageGetReady]
+const messageGetReady = {
+  sourceX: 134,
+  sourceY: 0,
+  width: 174,
+  height: 152,
+  posX: (canvas.width / 2) - 174 / 2,
+  posY: 50,
+  mDraw() {
+    context.drawImage(
+      sprites,
+      messageGetReady.sourceX, messageGetReady.sourceY, // Sprite X, Sprite Y
+      messageGetReady.width, messageGetReady.height, // Tamanho de recorte na Sprite
+      messageGetReady.posX, messageGetReady.posY, // Posição na tela
+      messageGetReady.width, messageGetReady.height // Tamanho da imagem na tela
+    );
+  }
+}
+
+// [Background]
+const background = {
   sourceX: 390,
   sourceY: 0,
   width: 275,
@@ -20,18 +43,18 @@ const backgroud = {
 
     context.drawImage(
       sprites,
-      backgroud.sourceX, backgroud.sourceY, // Sprite X, Sprite Y
-      backgroud.width, backgroud.height, // Tamanho de recorte na Sprite
-      backgroud.posX, backgroud.posY, // Posição na tela
-      backgroud.width, backgroud.height // Tamanho da imagem na tela
+      background.sourceX, background.sourceY, // Sprite X, Sprite Y
+      background.width, background.height, // Tamanho de recorte na Sprite
+      background.posX, background.posY, // Posição na tela
+      background.width, background.height // Tamanho da imagem na tela
     );
     
     context.drawImage(
       sprites,
-      backgroud.sourceX, backgroud.sourceY, // Sprite X, Sprite Y
-      backgroud.width, backgroud.height, // Tamanho de recorte na Sprite
-      (backgroud.posX + backgroud.width), backgroud.posY, // Posição na tela
-      backgroud.width, backgroud.height // Tamanho da imagem na tela
+      background.sourceX, background.sourceY, // Sprite X, Sprite Y
+      background.width, background.height, // Tamanho de recorte na Sprite
+      (background.posX + background.width), background.posY, // Posição na tela
+      background.width, background.height // Tamanho da imagem na tela
     );
   }  
 }
@@ -71,7 +94,19 @@ const flappyBird = {
   height: 24,
   posX: 10,
   posY: 50,
+  speedX: 0,
+  speedY: 1,
+  gravity: 0.25,
+  update() {
+    if(flappyBird.speedY < 1) {
+      flappyBird.speedY = flappyBird.speedY + 0.1;
+    }
+    flappyBird.speedY = flappyBird.speedY + flappyBird.gravity;
+    flappyBird.posX = flappyBird.posX + flappyBird.speedX;
+    flappyBird.posY = flappyBird.posY + flappyBird.speedY;
+  },
   mDraw() {
+    //flappyBird.update();
     context.drawImage(
       sprites,
       flappyBird.sourceX, flappyBird.sourceY, // Sprite X, Sprite Y
@@ -82,12 +117,52 @@ const flappyBird = {
   }
 }
 
+// [Screens]
+const Screens = {
+  START: {
+    click() {
+      changeToScreen(Screens.GAME)
+    },
+    mDraw() {
+      background.mDraw();
+      floor.mDraw();
+      flappyBird.mDraw();
+      messageGetReady.mDraw();
+    },
+    update() {}
+  }
+};
+
+Screens.GAME = {
+  click() {
+    flappyBird.speedY = -6
+  },
+  mDraw() {
+    background.mDraw();
+    floor.mDraw();
+    flappyBird.mDraw();
+  },
+  update(){
+    flappyBird.update();
+  }
+}
+
+function changeToScreen(newScreen) {
+  screenEnabled = newScreen;
+}
+
 function loop() {
-  backgroud.mDraw();
-  floor.mDraw();
-  flappyBird.mDraw();
+  screenEnabled.update();
+  screenEnabled.mDraw();
 
   requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function() {
+  if(screenEnabled.click) {
+    screenEnabled.click();
+  }
+});
+
+changeToScreen(Screens.START);
 loop();
